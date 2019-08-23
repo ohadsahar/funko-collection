48771ac6e5927e943875bb22fa3bfd427f4411ab
ohadsahar <46564719+ohadsahar@users.noreply.github.com>
2019-08-23 19:49:29 +0300

    continue later

--- funko-webapp/src/app/core/components/user-profile/user-profile.component.ts
+++ funko-webapp/src/app/core/components/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginService } from '../../services/login.service';
+import { PrivacySettings } from '../../../shared/interfaces/privacy-settings.interface';
 import { RegisterInterface } from '../../../shared/interfaces/register.interface';
+import { LoginService } from '../../services/login.service';
 import { MessageService } from '../../services/message.service';
 import { ShareDataService } from '../../services/share-data.service';
 
@@ -14,17 +15,28 @@ export class UserProfileComponent implements OnInit {
   favoritePop: string;
   age: number;
   userData: RegisterInterface;
+  privacySettings: PrivacySettings = new PrivacySettings(false, false, false, false, false, false);
   constructor(private loginService: LoginService, private messageService: MessageService,
               private shareDataService: ShareDataService) { }
 
   ngOnInit() {
     this.loginService.getUserDataByToken().subscribe(response => {
       this.userData = response.message;
+      this.privacySettings.showCollection = true;
+      this.privacySettings.showPersonalData = true;
+      this.privacySettings.canSendMessage = false;
+      this.privacySettings.friendShowCollection = false;
+      this.privacySettings.friendShowPersonalData = true;
+      this.privacySettings.friendCanSendMessage = true;
       this.shareDataService.changeUser(this.userData);
 
     }, (error) => {
       this.messageService.failedMessage(error);
     });
+  }
+
+  updateSettings() {
+    console.log(this.privacySettings);
   }
 
 }
---
+++ funko-webapp/src/app/shared/interfaces/privacy-settings.interface.ts
@@ -0,0 +1,5 @@
+export class PrivacySettings {
+  constructor(public showCollection: boolean, public showPersonalData: boolean,
+              public canSendMessage: boolean, public friendShowCollection: boolean, public friendShowPersonalData: boolean
+              ,public friendCanSendMessage: boolean) { }
+}
