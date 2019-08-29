5a521e7677e3de81be449da14a9e754226b3740a
ohadsahar <46564719+ohadsahar@users.noreply.github.com>
2019-08-29 14:39:36 +0300

    added conditions backend

--- funko-backend/src/components/account-setting/dto/recover-email.dto.ts
+++ funko-backend/src/components/account-setting/dto/recover-email.dto.ts
@@ -1,6 +1,18 @@
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator'
 export class RecoverMailDto {
+
+    @IsEmail()
     public email: string;
+
+    @IsString()
+    @IsNotEmpty()
     public password: string;
+
+    @IsString()
+    @IsNotEmpty()
     public firstname: string;
+
+    @IsString()
+    @IsNotEmpty()
     public lastname: string;
 }
